587178845896e80bfb592bb2b4448bcd606fca90
facundozerbino <facundozerbino@gmail.com>
2022-03-09 12:48:51 -0300

    1.16.4

--- src/util/Injected.js
+++ src/util/Injected.js
@@ -69,6 +69,9 @@ exports.ExposeStore = (moduleRaidStr) => {
 };
 
 exports.LoadUtils = () => {
+    if(!window.Store.genId) {
+        window.Store.genId = window.Store.MsgKey.newId;
+    }
     window.WWebJS = {};
 
     window.WWebJS.sendSeen = async (chatId) => {
